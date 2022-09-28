6812df861408c8cbf3e828359679018cd2dbcf7f
AdrielDV <adrielnds90@gmail.com>
2022-09-28 14:21:10 -0300

    first commit

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+public class PrimeiroPrograma{
+
+public static void main(String[] args){
+   int a = 2;
+ int b =4;
+int z = a+b;
+    System.out.println(z);
+   System.out.println("Hello World!");
+}
+}
